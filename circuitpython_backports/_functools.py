88248ddc741c4be397fda706e82c2062cea3dcfb
elpekenin <elpekenin@elpekenin.dev>
2024-07-15 22:50:36 +0200

    add `@functools.lru_cache` and `functools.reduce`

--- circuitpython_backports/_functools.py
+++ circuitpython_backports/_functools.py
@@ -1,3 +1,40 @@
+def lru_cache(maxsize=None):
+    """Add memoization to a function, with at most `maxsize` entries (unlimited if `None`).
+
+    When (if) maxsize is reached, the element who hasn't been used for the longest time is removed.
+
+    Note: Only some types of arguments will be supported, due to the basic args->key conversion.
+    """
+
+    def wrapper(func):
+        _cache = {}
+
+        def inner(*args, **kwargs):
+            key = str(args) + str(kwargs)
+
+            if key in _cache:
+                # edit dict to "move" key into most recent
+                # (last position)
+                ret = _cache.pop(key)
+                _cache[key] = ret
+                return ret
+
+            ret = func(*args, **kwargs)
+
+            # remove item if size reached
+            if len(_cache) == maxsize:
+                # first key == oldest
+                _cache.pop(_cache.keys()[0])
+
+            # save in cache
+            _cache[key] = ret
+            return ret
+
+        return inner
+
+    return wrapper
+
+
 class partial:
     """Create a new function by storing some arguments to be passed into
     an existing function.
@@ -11,6 +48,20 @@ class partial:
     def __call__(self, *args, **kwargs):
         kw = self._kwargs | kwargs
         return self._func(*self._args, *args, **kw)
+
+
+def reduce(func, iterable, initializer=None):
+    iterator = iter(iterable)
+
+    if initializer is None:
+        val = next(iterator)
+    else:
+        val = initializer
+
+    for i in iterator:
+        val = func(val, i)
+
+    return val
 
 
 def wraps(func):
